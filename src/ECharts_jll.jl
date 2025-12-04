7f08611f2cb8acbe86751bdc3bb2f58b4fc2cbf3
jlbuild <juliabuildbot@gmail.com>
2025-12-04 17:34:27 +0000

    ECharts_jll build 6.0.0+0

--- src/ECharts_jll.jl
+++ src/ECharts_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("ECharts")
-JLLWrappers.@generate_main_file("ECharts", UUID("ffd69456-1935-58d2-abba-ba12e8909167"))
+JLLWrappers.@generate_main_file("ECharts", Base.UUID("ffd69456-1935-58d2-abba-ba12e8909167"))
 end  # module ECharts_jll
